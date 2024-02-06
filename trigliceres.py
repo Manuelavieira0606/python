9b2d5e9a68a53395adc3c5b7f61b10f583e503ff
Manuelavieira0606 <manuela.rvieira09@gmail.com>
2024-02-06 15:34:30 -0300

    first commit

---
+++ trigliceres.py
@@ -0,0 +1,14 @@
+j = input("Você está de jejum s/n? ")
+t = input("Trigliceres? ")
+t = input(t) # converter para int
+if j == 's':
+    if t > 150:
+        print("Está alto!")
+    else: 
+        print("Está normal!")
+else:
+    if t > 175:
+        print("Está alto!")
+    else: 
+        print("Está normal!")
+            
